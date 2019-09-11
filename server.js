1a0d41176a524734d216ea3a42843eefa90a86be
Alex Spencer <alexander.j.spencer@gmail.com>
2019-09-11 14:38:26 -0700

    created index.js and imported server from server.js

--- server.js
+++ server.js
@@ -9,13 +9,15 @@
 
 const http = require('http'); //requires the http module that ships with Node.js, makes it callable. 
 
-function onRequest(req, res) {
-    console.log('Request received');
-    res.writeHead(200, { 'Content-Type': 'text/plain'});
-    res.write('Hello World');
-    res.end();
+function start() {
+    function onRequest(req, res) {
+        console.log('Request received');
+        res.writeHead(200, { 'Content-Type': 'text/plain'});
+        res.write('Hello World');
+        res.end();
+    }
+    http.createServer(onRequest).listen(8888);
+    console.log('Server has started');
 }
 
-http.createServer(onRequest).listen(8888);
-
-console.log('Server has started');
+exports.start = start;
