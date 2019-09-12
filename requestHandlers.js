44c0e810e97b9987fe25eed01441f2397b852990
Alex Spencer <alexander.j.spencer@gmail.com>
2019-09-12 09:24:03 -0700

    fixing blocking operations

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,20 +1,24 @@
 const exec = require('child_process').exec;
 
-function start() {
+function start(res) {
     console.log('Request handler \'start\' was called');
     const content = 'empty';
 
     exec('ls -lah', function(error, stdout, stderr) {
-        content = stdout;
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.write(stdout);
+        res.end();
     });
 
     return content;
     
 }
 
-function upload() {
+function upload(res) {
     console.log('Request handler \'upload\' was called');
-    return 'Hello Upload';
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('Hello Upload');
+    res.end();
 }
 
 exports.start = start;
