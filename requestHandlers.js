a07b70c7d1fbb6dd908613358f2c453c77250dae
Alex Spencer <alexander.j.spencer@gmail.com>
2019-09-12 09:46:41 -0700

    working POST

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,19 +1,20 @@
-const exec = require('child_process').exec;
+// const exec = require('child_process').exec;
+const querystring = require('querystring');
 
-function start(res) {
+function start(res, postData) {
     console.log('Request handler \'start\' was called');
 
-    const body = '<html>'+
-    '<head>'+
-    '<meta http-equiv="Content-Type" content="text/html; '+
-    'charset=UTF-8" />'+
-    '</head>'+
-    '<body>'+
-    '<form action="/upload" method="post">'+
-    '<textarea name="text" rows="20" cols="60"></textarea>'+
-    '<input type="submit" value="Submit text" />'+
-    '</form>'+
-    '</body>'+
+    const body = '<html>' +
+    '<head>' +
+    '<meta http-equiv="Content-Type" content="text/html; ' +
+    'charset=UTF-8" />' +
+    '</head>' +
+    '<body>' +
+    '<form action="/upload" method="post">' +
+    '<textarea name="text" rows="20" cols="60"></textarea>' +
+    '<input type="submit" value="Submit text" />' +
+    '</form>' +
+    '</body>' +
     '</html>';
 
     res.writeHead(200, { 'Content-Type': 'text/html' });
@@ -32,10 +33,11 @@ function start(res) {
     
 }
 
-function upload(res) {
+function upload(res, postData) {
     console.log('Request handler \'upload\' was called');
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write('Hello Upload');
+    res.write('You\'ve sent the text: ' +
+    querystring.parse(postData).text);
     res.end();
 }
 
--- server.js
+++ server.js
@@ -12,9 +12,22 @@ const url = require('url');
 
 function start(route, handle) {
     function onRequest(req, res) {
+        let postData = '';
         const pathname = url.parse(req.url).pathname;
         console.log('Request for ' + pathname + ' received');
-        route(handle, pathname, res);
+
+        req.setEncoding('utf8');
+
+        req.addListener('data', function(postDataChunk) {
+            postData += postDataChunk;
+            console.log('Received POST data chunk \'' +
+            postDataChunk + '\'.');
+        });
+
+        req.addListener('end', function() {
+            route(handle, pathname, res, postData);
+        });
+        // route(handle, pathname, res);
 
         // route(handle, pathname);
 
